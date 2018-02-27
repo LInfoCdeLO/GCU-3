ddf49bcb9b7d28b9cc536f3dfce679e5fd6945dc
Alban PRATS <alban.prats@gmail.com>
2018-02-27 15:23:47 +0100

    On test

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+public class Main extends JFrame{
+    public void main (String[] strings){
+        Timer time = new Timer();
+        System.out.println("coucou");
+        time.delay(50);
+        System.out.println("ah !");
+    }
+
+
 }
+
