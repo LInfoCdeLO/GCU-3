e839f4d69f18a5003e4129764282b5947ce75392
Alban PRATS <alban.prats@gmail.com>
2018-03-13 14:49:44 +0100

    on fait du ménage

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
-public class Main extends JFrame{
+
+public class Main extends JFrame {
     public void main (String[] strings){
-        Timer time = new Timer();
-        System.out.println("coucou");
-        time.delay(50);
-        System.out.println("ah !");
+
     }
 
 
---
+++ src/MainFenetre.java
@@ -0,0 +1,12 @@
+public class MainFenetre{
+  
+	public static void main(String[] args){
+	
+	Fenetre mafenetre=new Fenetre();
+	
+	
+	
+	
+		
+	}
+}
